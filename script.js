3790a82ef5b9eeca2e89fa1e1f0ef5ed2782ff01
Alyssa <alyssamc17@gmail.com>
2023-03-22 11:47:54 -0400

    Update script.js
    pokemon type added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ button.addEventListener('click', (e) => {
     const randomNumber = Math.ceil(Math.random() * 905)
     const nameElem = document.getElementById("name-" + i);
     const pokemonImage = document.getElementById('pokemon-' + i)
+    const typeElem = document.getElementById('type-' + i);
     const heightElem = document.getElementById("height-" + i);
     const weightElem = document.getElementById("weight-" + i);
     fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
@@ -17,11 +18,21 @@ button.addEventListener('click', (e) => {
         weight,
         sprites
       } = pokemon;
+
+      const types = pokemon["types"]
+      console.log(types[0])
+      const slot_1 = types[0]
+      console.log(slot_1["type"])
+      const characterType = slot_1["type"]
+      console.log(characterType["name"])
+      const typeName = characterType["name"]
+
       console.log(name)
       nameElem.innerHTML = name;
       heightElem.innerHTML = height;
       weightElem.innerHTML = weight;
       pokemonImage.setAttribute('src', sprites.front_default);
+      typeElem.innerText = typeName;
     })
   }
 })
